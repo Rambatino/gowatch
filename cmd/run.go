219be911821a56bb8b82881afbd8a64a27b8b604
Mark Ramotowski <mark.tint.ramotowski@gmail.com>
2019-05-08 13:53:30 +1200

    switched to single command args

--- cmd/run.go
+++ cmd/run.go
@@ -18,8 +18,8 @@ func init() {
 	runCmd.Flags().StringSliceVarP(&Extensions, "extensions", "e", []string{}, "Comma separated file extensions/types in which to search for. N.B. don't pass globs.")
 	runCmd.Flags().StringSliceVarP(&Paths, "paths", "p", []string{}, "Comma separated paths (folders and files) in which to search in. N.B. don't pass globs.")
 	runCmd.Flags().BoolVarP(&Recursive, "recursive", "r", false, "Whether to search recursively")
-	runCmd.Flags().StringSliceVarP(&IgnoreExtensions, "ignore-extensions", "ie", []string{}, "What file extensions to ignore. N.B. don't pass globs.")
-	runCmd.Flags().StringSliceVarP(&IgnorePaths, "ignore-paths", "ip", []string{}, "What file paths to ignore. N.B. don't pass globs.")
+	runCmd.Flags().StringSliceVarP(&IgnoreExtensions, "ignore-extensions", "i", []string{}, "What file extensions to ignore. N.B. don't pass globs.")
+	runCmd.Flags().StringSliceVarP(&IgnorePaths, "ignore-paths", "o", []string{}, "What file paths to ignore. N.B. don't pass globs.")
 	rootCmd.AddCommand(runCmd)
 }
 
